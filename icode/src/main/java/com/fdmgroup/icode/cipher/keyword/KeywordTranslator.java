a1863c00548c3eb12a779975109e88180ade3138
AlkwiseCodes <164106857+AlkwiseCodes@users.noreply.github.com>
2024-03-20 17:06:42 -0400

    Update KeywordTranslator.java, completed the encript/decrypt methods

--- icode/src/main/java/com/fdmgroup/icode/cipher/keyword/KeywordTranslator.java
+++ icode/src/main/java/com/fdmgroup/icode/cipher/keyword/KeywordTranslator.java
@@ -13,14 +13,49 @@ public class KeywordTranslator {
 	} 
 	
 	public static String encriptPhrase(String string) {
-		// TODO
-		return "Not Completed";
+		
+		//Base case
+		if(!isValidInput(string)) {
+			return "invalid";
+		}
+		StringBuilder encrypted = new StringBuilder();
+		for(char character : string.toCharArray()) {
+			if(character >= 'a' && character < 'z') {
+				encrypted.append((char) (character+1));
+			}
+			else if(character == 'z') {
+				encrypted.append('a');
+			}
+			else {
+				encrypted.append(character);
+			}
+		}
+		
+		return encrypted.toString();
 	}
 
+
+
 	public static String decriptPhrase(String string) {
-		// TODO
-		return "Not Completed";
+		if(!isValidInput(string)) {
+			return "Invaild input";
+		}
+		StringBuilder decrypted = new StringBuilder();
+		for(char character : string.toCharArray()) {
+			if(character > 'a' && character <= 'z') {
+				decrypted.append((char) (character-1));
+			}
+			else if(character == 'a') {
+				decrypted.append('z');
+			}
+			else {
+				decrypted.append((char) (character));
+			}
+		}
+		return decrypted.toString();
 	}
-	
-	// TODO - Create validation method
+	private static boolean isValidInput(String string) {
+		
+		return string != null && !string.isEmpty();
+	}
 }
