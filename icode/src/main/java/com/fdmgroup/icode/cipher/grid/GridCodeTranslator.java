fabc24985ddd075775153aa0116fcb1e1ffd60bb
AlkwiseCodes <164106857+AlkwiseCodes@users.noreply.github.com>
2024-03-20 17:05:32 -0400

    Update GridCodeTranslator.java, completed the two encryption methods

--- icode/src/main/java/com/fdmgroup/icode/cipher/grid/GridCodeTranslator.java
+++ icode/src/main/java/com/fdmgroup/icode/cipher/grid/GridCodeTranslator.java
@@ -1,15 +1,34 @@
 package com.fdmgroup.icode.cipher.grid;
 
 public class GridCodeTranslator {
-
+	/**
+	 * 
+	 * @param user input 
+	 * @return encoded user information
+	 */
 	public static String encodeUserInput(String input) {
-		// TODO 
-		return "Not Completed";
+		
+		StringBuilder encoded = new StringBuilder();
+		for(char character : input.toCharArray()) {
+			char shifted = (char) (character + 3); 
+			encoded.append(shifted);
+		}
+		
+		return encoded.toString();
 	}
-
+	/**
+	 * 
+	 * @param encoded user input
+	 * @return original user input
+	 */
 	public static String decodeUserInput(String input) {
-		// TODO 
-		return "Not Completed";
+		
+		StringBuilder decoded = new StringBuilder();
+		for(char character : input.toCharArray()) {
+			char shifted = (char) (character - 3);
+			decoded.append(shifted);
+		}
+		return decoded.toString();
 	}
 
 }
